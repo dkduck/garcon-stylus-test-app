51640915dd914e1bd510d7a01e7f5684541f03eb
Florian Kugler <mail@floriankugler.de>
2011-12-13 19:52:13 +0100

    changed server config

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ var myApp = g.App.create({
   theme: 'sc-theme', // what theme to use, will be the class of the body tag
   htmlHead: '<title>Docentending</title>', // what tags to include in the header of the generated html
 
-  minifyScripts: true,
-  minifyStylesheets: true,
+//  minifyScripts: true,
+//  minifyStylesheets: true,
 
   hasSC: true, // an app will have SC by default, if you don't want this, set to false
   configSC: {
